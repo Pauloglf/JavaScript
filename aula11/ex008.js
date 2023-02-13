1a30bdeb0c5cd481320bf81d741b575582160785
Pauloglf <96998821+Pauloglf@users.noreply.github.com>
2023-02-13 18:38:53 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60){
+    console.log('Ultrapassou a velocidade permitida. MULTA: 150.00R$')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+console.log(`Se você vive em ${país}`)
+if(país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é Estrangeiro!')
+}
