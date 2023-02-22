e5446a514de7ea26066943930d1aea650620bf23
Pauloglf <96998821+Pauloglf@users.noreply.github.com>
2023-02-22 19:00:43 -0300

    ex14-1

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,32 @@
-alert('Olá!')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('passo invalido! passou a ser 1')
+            p = 1
+        }
+        if (i < f) {
+            // crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c}\u{27A1}`   
+            }
+        } else {
+            // regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c}\u{27A1}`
+            } 
+        }
+        res.innerHTML += `\u{1F3F4}`    
+    }
+}
