846a8368fd5f576c1d41d230295c44318f1c267e
Pauloglf <96998821+Pauloglf@users.noreply.github.com>
2023-02-23 18:50:30 -0300

    ex14-2

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+    let con = document.querySelector('input#con')
+    if (num.value.length == 0) {
+        window.alert('Por Favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let t = Number(con.value)
+        tab.innerHTML = ""
+        for(let c = 1; c <= t; c++){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
