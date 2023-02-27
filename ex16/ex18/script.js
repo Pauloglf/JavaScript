a6d42608d76df0b789273e8a08d8142808e023eb
Pauloglf <96998821+Pauloglf@users.noreply.github.com>
2023-02-27 17:41:36 -0300

    exx18-1

--- ex16/ex18/script.js
+++ ex16/ex18/script.js
@@ -3,11 +3,27 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let val = []
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value)) {
-        
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true 
     } else {
-        
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, val)) {
+        window.alert('Tudo Ok!')
+    } else {
+       window.alert('Valor invalido ou já na lista.')
     }
     
 }
