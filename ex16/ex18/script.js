22fa591d61c85156e5d6af6dbd85426e173d76f2
Pauloglf <96998821+Pauloglf@users.noreply.github.com>
2023-02-28 18:11:43 -0300

    ex16-2

--- ex16/ex18/script.js
+++ ex16/ex18/script.js
@@ -21,9 +21,42 @@ function inLista(n, l) {
 
 function adicionar() {
     if (isNumero(num.value) && !inLista(num.value, val)) {
-        window.alert('Tudo Ok!')
-    } else {
+        val.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+        } else {
        window.alert('Valor invalido ou já na lista.')
     }
-    
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (val.length == 0) {
+        window.alert('Adicione valores primeiro')
+    } else {
+        let tot = val.length
+        let maior = val[0]
+        let menor = val[0]
+        let soma = 0
+        let media = 0
+        for(let pos in val) {
+            soma += val[pos]
+            if (val[pos] > maior) {
+                maior = val[pos]
+            }
+            if (val[pos] < menor) {
+                menor = val[pos]
+            }
+        }
+        média = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números.</p>`
+        res.innerHTML += `<p>o maior valor encontrado foi ${maior}.</p>`
+        res.innerHTML += `<p>o menor valor encontrado foi ${menor}.</p>`
+        res.innerHTML += `<p>somando tudo, o resultado é ${soma}.</p>`
+        res.innerHTML += `<p>a média encontrada foi ${média}</p>`
+    }
 }
